4ed99eb3fb59112e2332788c726a8143f1b4862b
K4Kabir <kabirmalv@gmail.com>
2025-07-28 08:36:52 +0530

    vv

--- app/(main)/dashboard/[id]/page.tsx
+++ app/(main)/dashboard/[id]/page.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-<<<<<<< HEAD
 import React from "react";
-=======
-import React, { useState } from "react";
->>>>>>> 24629380938fddfd4049c68820cc7f40393d6d6c
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Button } from "@/components/ui/button";
@@ -12,10 +8,7 @@ import { Progress } from "@/components/ui/progress";
 import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
 import { Label } from "@/components/ui/label";
-<<<<<<< HEAD
 import { useUser } from "@clerk/nextjs";
-=======
->>>>>>> 24629380938fddfd4049c68820cc7f40393d6d6c
 
 interface PageProps {
     params: {
@@ -23,7 +16,6 @@ interface PageProps {
     };
 }
 
-<<<<<<< HEAD
 interface options {
     title: string
     totalVotes: number
@@ -40,9 +32,6 @@ interface poll {
 }
 
 
-=======
-
->>>>>>> 24629380938fddfd4049c68820cc7f40393d6d6c
 const isPollActive = (validTill: string) => {
     const now = new Date();
     return new Date(validTill) > now;
@@ -50,7 +39,6 @@ const isPollActive = (validTill: string) => {
 
 export default function Page({ params }: PageProps) {
     const { id } = React.use(params);
-<<<<<<< HEAD
     const poll: poll = useQuery(api.polls.getPollById, { id });
     const totalVotes = poll?.options.reduce((sum, option) => sum + option?.totalVotes, 0)
     const { isLoaded, user, isSignedIn } = useUser();
@@ -63,11 +51,6 @@ export default function Page({ params }: PageProps) {
     }
 
 
-=======
-    const poll = useQuery(api.polls.getPollById, { id });
-    console.log(poll)
-
->>>>>>> 24629380938fddfd4049c68820cc7f40393d6d6c
 
     return (
         <main className="max-w-3xl mx-auto p-6">
@@ -75,7 +58,6 @@ export default function Page({ params }: PageProps) {
                 <CardHeader>
                     <div className="flex justify-between items-center">
                         <h1 className="text-2xl font-bold">{poll?.title}</h1>
-<<<<<<< HEAD
                     </div>                    <h1>{poll?.description}</h1>
                     <RadioGroup className="py-5 flex flex-col gap-5">
                         {
@@ -86,17 +68,6 @@ export default function Page({ params }: PageProps) {
                                         <Label className="text-xl" htmlFor={index + option?.title}>{option?.title}</Label>
                                         <Progress max={totalVotes} value={option?.totalVotes} />
                                         {option?.totalVotes}
-=======
-                    </div>
-                    <h1>{poll?.description}</h1>
-                    <RadioGroup className="py-5 flex flex-col gap-5">
-                        {
-                            poll?.options?.map((option, index) => {
-                                return (
-                                    <div key={index} className="flex items-center gap-3">
-                                        <RadioGroupItem value={option?.title} id={index} />
-                                        <Label className="text-xl" htmlFor={index}>{option?.title}</Label>
->>>>>>> 24629380938fddfd4049c68820cc7f40393d6d6c
                                     </div>
 
                                 )
